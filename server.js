a14a157f1b9139352e37adf7ef6016a31ed53562
NQNam <namtv27072001@gmail.com>
2022-01-05 15:50:47 +0700

    update

--- server.js
+++ server.js
@@ -1,5 +1,65 @@
-import express from "express"
-const app = express()
-const port = 3000
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = process.env.port || 5000;
+
+const URI =
+  "mongodb+srv://admin:admin123@cluster0.xvdds.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+app.use(bodyParser.json({ limit: "30mb " }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
+
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to DB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("err", err);
+  });
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+      default: "Anonymous",
+    },
+    attachment: String,
+    likeCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
 
 
+const myBlog = mongoose.model('post', blogSchema);
+
+
+app.get("/" , async (req , res) => {
+    try {
+        const posts = await myBlog.find({});
+        console.log("Post" , posts);
+        res.status(200).json(posts)
+    } catch (err) {
+        res.status(500).json({error : err})
+    }
+});
