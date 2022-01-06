0002cc0d609319bbc4009c6817ce2198a7b6fbfc
NQNam <namtv27072001@gmail.com>
2022-01-06 08:00:18 +0700

    update

--- server.js
+++ server.js
@@ -7,59 +7,59 @@ app.get('/', (req , res) =>{
     res.json("SUCCESS")
 })
 
-// const URI =
-//   "mongodb+srv://admin:admin123@cluster0.xvdds.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const URI =
+  "mongodb+srv://admin:admin123@cluster0.xvdds.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-// app.use(bodyParser.json({ limit: "30mb " }));
-// app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
+app.use(bodyParser.json({ limit: "30mb " }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
 
-// mongoose
-//   .connect(URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("Connected to DB");
-//   })
-//   .catch((err) => {
-//     console.log("err", err);
-//   });
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log("err", err);
+  });
 
-// const blogSchema = new mongoose.Schema(
-//   {
-//     title: {
-//       type: String,
-//       required: true,
-//     },
-//     content: {
-//       type: String,
-//       required: true,
-//     },
-//     author: {
-//       type: String,
-//       required: true,
-//       default: "Anonymous",
-//     },
-//     attachment: String,
-//     likeCount: {
-//       type: Number,
-//       default: 0,
-//     },
-//   },
-//   { timestamps: true }
-// );
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+      default: "Anonymous",
+    },
+    attachment: String,
+    likeCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
 
-// const myBlog = mongoose.model("post", blogSchema);
+const myBlog = mongoose.model("post", blogSchema);
 
-// app.get("/posts", async (req, res) => {
-//   try {
-//     const posts = await myBlog.find({});
-//     console.log("Post", posts);
-//     res.status(200).json(posts);
-//   } catch (err) {
-//     res.status(500).json({ error: err });
-//   }
-// });
+app.get("/posts", async (req, res) => {
+  try {
+    const posts = await myBlog.find({});
+    console.log("Post", posts);
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
 
 app.listen(process.env.PORT, () => {
     console.log(process.env.PORT);
